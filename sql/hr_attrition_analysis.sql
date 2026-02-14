68a3756ff64276e6f00d68503b6af89a9000b7af
Garv <macbook@macbooks-MacBook-Pro.local>
2026-02-14 09:40:44 +0530

    Initial commit: HR Attrition Analysis project

---
+++ sql/hr_attrition_analysis.sql
@@ -0,0 +1,31 @@
+-- View total employees and attrition rate
+SELECT 
+    COUNT(*) AS total_employees,
+    SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) AS employees_left,
+    ROUND(
+        (SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) * 100.0) / COUNT(*),
+        2
+    ) AS attrition_rate_percentage
+FROM hr_attrition;
+
+
+-- Attrition by Department
+SELECT 
+    Department,
+    COUNT(*) AS total_employees,
+    SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) AS employees_left,
+    ROUND(
+        (SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) * 100.0) / COUNT(*),
+        2
+    ) AS attrition_rate
+FROM hr_attrition
+GROUP BY Department
+ORDER BY attrition_rate DESC;
+
+
+-- Attrition by Job Role
+SELECT 
+    JobRole,
+    COUNT(*) AS total_employees,
+    SUM(CASE WHEN Attrition = 'Yes' THEN 1 ELSE 0 END) AS employees_left,
+    ROUND(
